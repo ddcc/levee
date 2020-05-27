294cff26ebf0b8b99fc1991e3a0c6fa6addf75f2
Dominic Chen <d.c.ddcc@gmail.com>
2020-05-27 06:04:02 -0400

    force gold as default linker

--- tools/clang/lib/Driver/ToolChains.cpp
+++ tools/clang/lib/Driver/ToolChains.cpp
@@ -2275,7 +2275,7 @@ Linux::Linux(const Driver &D, const llvm::Triple &Triple, const ArgList &Args)
   PPaths.push_back(Twine(GCCInstallation.getParentLibPath() + "/../" +
                          GCCInstallation.getTriple().str() + "/bin").str());
 
-  Linker = GetProgramPath("ld");
+  Linker = GetProgramPath("gold");
 
   Distro Distro = DetectDistro(Arch);
 
